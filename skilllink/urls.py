84165b786d8320305b89c182035c38cf6b82244b
HUAWEI2O1 <162959370+HUAWEI2O1@users.noreply.github.com>
2026-03-31 02:48:42 +0800

    Add complete WorkerProfile fields, update views and urls

--- skilllink/urls.py
+++ skilllink/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for skilllink project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from django.contrib import admin
 from core import views
@@ -21,10 +5,17 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Workers
     path('api/workers/', views.get_workers),
+    path('api/workers/<int:pk>/', views.get_worker),
+
+    # Jobs
+    path('api/jobs/', views.get_jobs),
     path('api/jobs/create/', views.create_job),
-    
-    # New Auth Endpoints
+
+    # Auth
     path('api/register/', views.register_user),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
